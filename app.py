825fa006e0729bcedf311ff50a8439f2bcb52de2
Александр А. Кравченко <kravchenko.a@local.pulsar>
2020-01-22 13:27:31 +0300

    Snake...

---
+++ app.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from PIL import Image, ImageTK
+
+class Snake(tk.Canvas):
+	def __init__(self):
+		super().__init__(width = 600, height = 620, background = "black", highlightthickness = 0)
+
+		self.snake_positions = [(100, 100), (80, 100), (60, 100)]
+
+
+
+
+		self.load.assets()
+		self.create_objects()
+
+	def load_assets(self):
+		try:
+			self.snake_body_image = Image.open("./assets/snake.png")
+			self.snake_body_image = ImageTK.PhotoImage(self.snake_body_image)
+
+			self.snake_food_image = Image.open("./assets/food.png")
+			self.snake_food_image = ImageTK.PhotoImage(self.snake_food_image)
+		except IOError as error:
+			print(error)
+			root.destroy() 
+
+	def create_objects(self):
+		for x_position, y_position in self.snake_positions:
+			self.create_image(x_position, y_position, image = self.snake_body, tag = 'snake')
+
+
+
+
+root = tk.Tk()
+root.title("Snake")
+root.resizable(False, False)
+
+board = Snake()
+board.pack()
+
+
+root.mainloop()
